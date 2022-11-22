bb9ab8f7764dd9744edfbeae2859a23295667175
Lorentz <91217473+Lauro235@users.noreply.github.com>
2022-11-22 13:17:27 +0000

    complete challenge!

--- main.js
+++ main.js
@@ -6,19 +6,29 @@ var fibsFizzBuzz = function(n) {
 implementation from https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
 */
 
-let arr = []
 
 function fibonacci(num){
-    var a = 1, b = 0, temp;
-  
-    while (num >= 0){
+  var a = 1, b = 0, temp;
+  let arr = []
+    while (num >= 1){
       temp = a;
       a = a + b;
       b = temp; // b updates
       arr.push(b)
       num--;
     }
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+        arr[i] = 'FizzBuzz'
+      }
+      if (arr[i] % 5 === 0) {
+        arr[i] = "Buzz"
+      }
+      if (arr[i] % 3 === 0) {
+        arr[i] = "Fizz"
+      }
+    }
+    return arr
   }
 
-  fibonacci(5)
-  console.log(arr);
+  console.log(fibonacci(20))
